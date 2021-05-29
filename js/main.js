a85b8b1adf9f076ae09b66f4cd6e808311a492d7
Sergey Antipov <72650361+AntipovSergey@users.noreply.github.com>
2021-05-29 13:30:42 +0300

    Исправляет ошибки ESlint и переименовывает вторую функцию в getRandomFloat

--- js/main.js
+++ js/main.js
@@ -3,29 +3,29 @@ const getRandomInteger = function (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   if (max <= min) {
-    return console.log('Максимальное значение должно быть больше минимального!');
+    return 'Максимальное значение должно быть больше минимального!';
   }
 
   if (min >= 0) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-    console.log('Диапазон может быть только положительный, включая ноль!');
-}
+  return 'Диапазон может быть только положительный, включая ноль!';
+};
 
 getRandomInteger (0, 6);
 
 //Для написания данной функции воспользовался кодом с сайта MDN
-const getRandomInteger = function (min, max, float) {
+const getRandomFloat = function (min, max, float) {
   min = Math.ceil(min);
   max = Math.floor(max);
   if (max <= min) {
-    return console.log('Максимальное значение должно быть больше минимального!');
+    return 'Максимальное значение должно быть больше минимального!';
   }
 
   if (min >= 0) {
     return Number((Math.random() * (max - min + 1) + min).toFixed(float));
   }
-    console.log('Диапазон может быть только положительный, включая ноль!');
-}
+  return 'Диапазон может быть только положительный, включая ноль!';
+};
 
-getRandomInteger (0, 6);
+getRandomFloat (0, 6);

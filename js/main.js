fd0162b19f12916f98a4089da2d47a0fead8bafe
Sergey Antipov <72650361+AntipovSergey@users.noreply.github.com>
2021-06-06 16:43:19 +0300

    Генерирует временные данные

--- js/main.js
+++ js/main.js
@@ -16,8 +16,6 @@ getRandomInteger (0, 6);
 
 //Для написания данной функции воспользовался кодом с сайта MDN
 const getRandomFloat = (min, max, float) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
   if (max <= min) {
     return 'Максимальное значение должно быть больше минимального!';
   }
@@ -29,3 +27,61 @@ const getRandomFloat = (min, max, float) => {
 };
 
 getRandomFloat (0, 6, 2);
+
+//Задание 4-го раздела
+
+const TYPE = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+
+const CHECK_IN = ['12:00', '13:00', '14:00'];
+
+const CHECK_OUT = ['12:00', '13:00', '14:00'];
+
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+
+const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+
+const SIMILAR_ADVIRTISMENTS = 10;
+
+
+//Функция, выбирающая случайный элемент из массива
+const getRandomArrayElement = (elements) => {
+  return elements[getRandomInteger(0, elements.length - 1)];
+};
+
+//Код функции по созданию нового массива с рандомными значениями из предложенного массива взят с сайта https://qna.habr.com/q/844269
+const createArr = ([...source], maxLength) => Array.from(
+  { length: Math.min(source.length, 1 + Math.random() * maxLength | 0) },
+  () => source.splice(Math.random() * source.length | 0, 1)[0],
+);
+
+//Функция, возвращающая объявления
+const createAdvertisment = () => {
+  return {
+    author: {
+      avatar: 'img/avatars/user' + 0 + getRandomInteger (1, 8) + '.png',
+    },
+    offer: {
+      title: 'Объявление о сдаче помещения',
+      address: getRandomFloat(10, 60, 5) + ', ' + getRandomFloat(10, 60, 5),
+      price: getRandomInteger (500, 30000),
+      type: getRandomArrayElement(TYPE),
+      rooms: getRandomInteger(1, 4),
+      guests: getRandomInteger(1, 6),
+      checkin: getRandomArrayElement(CHECK_IN),
+      checkout: getRandomArrayElement(CHECK_OUT),
+      features: createArr(FEATURES, getRandomInteger(0, 6)),
+      description: 'Описание помещения',
+      photos: createArr(PHOTOS, getRandomInteger(0, 3)),
+    },
+    location: {
+      lat: getRandomFloat(35.65000, 35.70000, 5),
+      lng: getRandomFloat(139.70000,139.80000, 5),
+    },
+  };
+};
+createAdvertisment();
+
+//Создаем 10 объявлений
+const similarAdvertisments = new Array(SIMILAR_ADVIRTISMENTS).fill(null).map(() => createAdvertisment());
+
+console.log(similarAdvertisments);

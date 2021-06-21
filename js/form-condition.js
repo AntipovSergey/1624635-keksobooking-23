0a6a3ec67160d570d4724ee72d66b506a3cb0996
Sergey Antipov <72650361+AntipovSergey@users.noreply.github.com>
2021-06-21 19:46:20 +0300

    Выполняет задание Отрисуй меня полностью (часть 2) по созданию функций, которые переводят формы map и notice в активное/неактивное состояние

---
+++ js/form-condition.js
@@ -0,0 +1,29 @@
+const advertisementForm = document.querySelector('.ad-form');
+const advertisementFormElements = advertisementForm.children;
+const advertisementMap = document.querySelector('.map__filters');
+const advertisementMapElements = advertisementMap.children;
+
+const formInactiveConditionHandler = () => {
+  advertisementForm.classList.add('ad-form--disabled');
+  for (let index = 0; index < advertisementFormElements.length; index++) {
+    advertisementFormElements[index].setAttribute('disabled', 'disabled');
+  }
+  advertisementMap.classList.add('ad-form--disabled');
+  for (let index = 0; index <  advertisementMapElements.length; index++) {
+    advertisementMapElements[index].setAttribute('disabled', 'disabled');
+  }
+};
+
+const formActiveConditionHandler = () => {
+  advertisementForm.classList.remove('ad-form--disabled');
+  for (let index = 0; index < advertisementFormElements.length; index++) {
+    advertisementFormElements[index].removeAttribute('disabled', 'disabled');
+  }
+  advertisementMap.classList.remove('ad-form--disabled');
+  for (let index = 0; index <  advertisementMapElements.length; index++) {
+    advertisementMapElements[index].removeAttribute('disabled', 'disabled');
+  }
+};
+
+formInactiveConditionHandler();
+formActiveConditionHandler();
--- js/main.js
+++ js/main.js
@@ -2,3 +2,4 @@ import './utils.js';
 import './create-advertisement.js';
 import './generate-similar-elements.js';
 import './form.js';
+import './form-condition.js';

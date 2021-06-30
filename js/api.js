a064b264b50410c967f17492732c6e2239638cf3
Sergey Antipov <72650361+AntipovSergey@users.noreply.github.com>
2021-06-30 14:15:55 +0300

    Добавляет оповещение об ошибке при неправильной загрузке данных с сервера

--- js/api.js
+++ js/api.js
@@ -1,8 +1,13 @@
+import {showAlert} from './utils.js';
+
 const getData = (onSuccess) => {
   fetch('https://23.javascript.pages.academy/keksobooking/data')
     .then((response) => response.json())
     .then((advertisements) => {
       onSuccess(advertisements);
+    })
+    .catch(() => {
+      showAlert('При загрузке данных с сервера произошла ошибка!');
     });
 };
 
--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,5 @@
+import {ALERT_SHOW_TIME} from './data.js';
+
 //Для написания данной функции воспользовался кодом с сайта MDN
 function getRandomInteger(min, max) {
   min = Math.ceil(min);
@@ -46,4 +48,25 @@ const getValueTypeOffer = (type) => {
   }
 };
 
-export {getRandomInteger, getRandomFloat, shuffle, getRandomArrayElement, getValueTypeOffer};
+const showAlert = (message) => {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = 100;
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.left = 0;
+  alertContainer.style.top = 0;
+  alertContainer.style.right = 0;
+  alertContainer.style.padding = '10px 3px';
+  alertContainer.style.fontSize = '30px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = 'red';
+
+  alertContainer.textContent = message;
+
+  document.body.append(alertContainer);
+
+  setTimeout(() => {
+    alertContainer.remove();
+  }, ALERT_SHOW_TIME);
+};
+
+export {getRandomInteger, getRandomFloat, shuffle, getRandomArrayElement, getValueTypeOffer, showAlert};
